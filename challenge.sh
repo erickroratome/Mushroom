4f4a7f019cab0594a30cb586dedc1da02e7ae411
JosucaZ <102182842+JosucaZ@users.noreply.github.com>
2023-09-25 14:09:53 -0300

    Update challenge.sh

--- challenge.sh
+++ challenge.sh
@@ -34,7 +34,7 @@ else
         echo -e "auditd nÃ£o instalado!\n"
         sleep 2
         echo -e "Deseja instalar?\n"
-        read -p "[S]im | [N]Ã£o: " resp
+        read -p "[S]im | [N]ao: " resp
         if [ $resp = "S" ] || [ $resp = "s" ]; then
                 echo ""
                 apt update -y
@@ -46,37 +46,28 @@ else
 fi
 #=======================================================|
 
+#CRIANDO ARQUIVOS HONEYFILE=============================|
+
 sudo auditctl -w /home/##a -p wa -k mush 2>/dev/null
 sudo auditctl -w /boot/##a -p wa -k mush 2>/dev/null
 sudo auditctl -w /etc/##a -p wa -k mush 2>/dev/null
 sudo auditctl -w /etc/ssh/##a -p wa -k mush 2>/dev/null
 sudo auditctl -w /usr/##a -p wa -k mush 2>/dev/null
 
-#CRIANDO ARQUIVOS HONEYFILE=============================|
-
-
-#LIMPANDO LOG===========================================|
-
-#sudo systemctl stop auditd.service
-#sudo systemctl start auditd.service
-
-#=======================================================|
-
-
 usuarios=$(cat /etc/passwd | grep -i /home | cut -d: -f1)
-if [ -e /home/##a ] || [ -e /boot/##a ] || [ -e /etc/##a ] || [ -e /etc/ssh/##a ] || [ -e /usr/##a ]; then
+if [ -e /home/1@11a.dat ] || [ -e /boot/1@11a.dat ] || [ -e /etc/1@11a.dat ] || [ -e /etc/ssh/1@11a.dat ] || [ -e /usr/1@11a.dat ]; then
         echo ""
 else
-        touch /home/##a 2>/dev/null
-        chmod 777 /home/##a
-        touch /boot/##a 2>/dev/null
-        chmod 777 /boot/##a
-        touch /etc/##a 2>/dev/null
-        chmod 777 /etc/##a
-        touch /etc/ssh/##a 2>/dev/null
-        chmod 777 /etc/ssh/##a
-        touch /usr/##a 2>/dev/null
-        chmod 777 /usr/##a
+        touch /home/1@11a.dat 2>/dev/null
+        chmod 777 /home/1@11a.dat
+        touch /boot/1@11a.dat 2>/dev/null
+        chmod 777 /boot/1@11a.dat
+        touch /etc/1@11a.dat 2>/dev/null
+        chmod 777 /etc/1@11a.dat
+        touch /etc/ssh/1@11a.dat 2>/dev/null
+        chmod 777 /etc/ssh/1@11a.dat
+        touch /usr/1@11a.dat 2>/dev/null
+        chmod 777 /usr/1@11a.dat
 fi
 
 for i in ${usuarios[@]}; do
@@ -87,47 +78,35 @@ for i in ${usuarios[@]}; do
 	sudo -u $i mkdir /home/$i/Desktop 2>/dev/null
 	sudo -u $i mkdir /home/$i/Videos 2>/dev/null
 
-        sudo -u $i touch /home/$i/##a 2>/dev/null
-        sudo -u $i touch /home/$i/Documents/##a 2>/dev/null
-        sudo -u $i touch /home/$i/Downloads/##a 2>/dev/null
-        sudo -u $i touch /home/$i/Desktop/##a 2>/dev/null
-        sudo -u $i touch /home/$i/Videos/##a 2>/dev/null
+        sudo -u $i touch /home/$i/1@11a.dat 2>/dev/null
+        sudo -u $i touch /home/$i/Documents/1@11a.dat 2>/dev/null
+        sudo -u $i touch /home/$i/Downloads/1@11a.dat 2>/dev/null
+        sudo -u $i touch /home/$i/Desktop/1@11a.dat 2>/dev/null
+        sudo -u $i touch /home/$i/Videos/1@11a.dat 2>/dev/null
 
-        chmod 777 /home/$i/##a 2>/dev/null
-        chmod 777 /home/$i/Documents/##a 2>/dev/null
-        chmod 777 /home/$i/Downloads/##a 2>/dev/null
-        chmod 777 /home/$i/Desktop/##a 2>/dev/null
-        chmod 777 /home/$i/Videos/##a 2>/dev/null
+        chmod 777 /home/$i/1@11a.dat 2>/dev/null
+        chmod 777 /home/$i/Documents/1@11a.dat 2>/dev/null
+        chmod 777 /home/$i/Downloads/1@11a.dat 2>/dev/null
+        chmod 777 /home/$i/Desktop/1@11a.dat 2>/dev/null
+        chmod 777 /home/$i/Videos/1@11a.dat 2>/dev/null
 
 
-        sudo auditctl -w /home/$i/##a -p wa -k mush 2>/dev/null
-        sudo auditctl -w /home/$i/Desktop/##a -p wa -k mush 2>/dev/null
-        sudo auditctl -w /home/$i/Videos/##a -p wa -k mush 2>/dev/null
-        sudo auditctl -w /home/$i/Downloads/##a -p wa -k mush 2>/dev/null
-        sudo auditctl -w /home/$i/Documents/##a -p wa -k mush 2>/dev/null
+        sudo auditctl -w /home/$i/1@11a.dat -p wa -k mush 2>/dev/null
+        sudo auditctl -w /home/$i/Desktop/1@11a.dat -p wa -k mush 2>/dev/null
+        sudo auditctl -w /home/$i/Videos/1@11a.dat -p wa -k mush 2>/dev/null
+        sudo auditctl -w /home/$i/Downloads/1@11a.dat -p wa -k mush 2>/dev/null
+        sudo auditctl -w /home/$i/Documents/1@11a.dat -p wa -k mush 2>/dev/null
 done
 #=======================================================|
 
-PidAnti=$(ps aux | grep -i challenge.sh | grep -i sudo | awk '{print $2}')
+#IDENTIFICANDO PID E MATANDO ===========================|
+
+#PidAnti=$(ps aux | grep -i challenge.sh | grep -i sudo | awk '{print $2}')
 echo "PidAnti: $PidAnti"
 PidAnti2=$(ps aux | grep -i challenge.sh | grep -i bash | awk '{print $2}')
 echo "PidAnti2: $PidAnti2"
-#sleep 100000000
-
-#arqMonitorados=(
-#  "/boot/##a"
-#  "/home/##a"
-#  "/etc/##a"
-#  "/etc/ssh/#aa"
-#  "/usr/##a"
-#)
-
-#for usuario in $usuarios; do
-#    arqMonitorados+=("/home/$usuario/##a")
-#done
 
 while true; do
-#  for arquivo in "${arqMonitorados[@]}"; do
       PID=$(sudo ausearch -k mush | grep -i ppid | tail -n 1 | grep -o 'pid=[0-9]*' | awk -F'=' '{print $2}')
       for i in $PID; do
         if [ $i = "$PidAnti" ] || [ $i = "$PidAnti2" ]; then
@@ -141,4 +120,3 @@ while true; do
       done
    sleep 0.2
   done
-#done
