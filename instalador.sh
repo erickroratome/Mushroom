28574525a102c539bca43bba8f9d3eb0df767e25
JosucaZ <102182842+JosucaZ@users.noreply.github.com>
2023-09-29 14:38:09 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -547,15 +547,17 @@ diretorio_base="/home"
 if [ -d "$diretorio_base" ]; then
 	diretorios_encontrados=($(listar_diretorios "$diretorio_base"))
 	for diretorio in "${diretorios_encontrados[@]}"; do
-    		if [[ $(stat -c %s "$diretorio/$nomearq") != $tamanhoHoneyfile ] || [ $(stat -c %s "$diretorio/$nomearq") != "$tamanhoHoneyfile-less" ]] && [ ! -e "$diretorio/$nomearq" ]; then
-      			touch ./SINALIZADOR.dat
-			echo "~# cp ./honeyfile.txt "$diretorio/$nomearq""
-			cp ./honeyfile-less.txt "$diretorio/$nomearq"
-   
-      			echo "~# sudo auditctl -w "$diretorio/$nomearq" -p wa -k mush"
-	        	sudo auditctl -w "$diretorio/$nomearq" -p wa -k mush 2>/dev/null
-			rm -rf ./SINALIZADOR.dat
-      		fi
+ 		if [ -e "$diretorio/$nomearq" ]; then
+    			if [ $(stat -c %s "$diretorio/$nomearq") != $tamanhoHoneyfile ] || [ $(stat -c %s "$diretorio/$nomearq") != "$tamanhoHoneyfile-less" ]; then
+      				touch ./SINALIZADOR.dat
+				echo "~# cp ./honeyfile.txt "$diretorio/$nomearq""
+				cp ./honeyfile-less.txt "$diretorio/$nomearq"
+   	
+   	   			echo "~# sudo auditctl -w "$diretorio/$nomearq" -p wa -k mush"
+		        	sudo auditctl -w "$diretorio/$nomearq" -p wa -k mush 2>/dev/null
+				rm -rf ./SINALIZADOR.dat
+	      		fi
+	 	fi
 	done
 fi
 
