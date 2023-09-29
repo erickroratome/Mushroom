1c87c309f6b5cbef4c7809135df6a3c924356129
JosucaZ <102182842+JosucaZ@users.noreply.github.com>
2023-09-29 10:59:03 -0300

    Update challenge.sh

--- challenge.sh
+++ challenge.sh
@@ -8,43 +8,6 @@ echo "| |_| | (_) \__ \  __/ |  | | (__|   <  "
 echo " \___/ \___/|___/\___|_|  |_|\___|_|\_\ "
 echo ""
 
-
-
-#DETECTAR ROOT===========================================|
-#valid=True
-#valid1=True
-#if [ $EUID != 0 ]; then
-#        valid=False
-#fi
-#if [ $USER != "root" ]; then
-#        valid1=False
-#fi
-#
-#if [ $valid = False ] && [ $valid1 = False ]; then
-#        echo -e "\nPor favor execute como root...\n"
-#        exit
-#fi
-#=======================================================|
-
-#echo -e "\nADICIONANDO REGRAS AUDITCTL..."
-#sudo auditctl -w /aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#sudo auditctl -w /home/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#sudo auditctl -w /etc/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#sudo auditctl -w /usr/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#sudo auditctl -w /backup/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#
-#usuarios=$(cat /etc/passwd | grep -i /home | cut -d: -f1)
-#
-#for i in ${usuarios[@]}; do
-#        sudo auditctl -w /home/$i/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#        sudo auditctl -w "$desktop/aaaaaaaa.txt" -p wa -k mush 2>/dev/null
-#        sudo auditctl -w /home/$i/$videos/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#        sudo auditctl -w /home/$i/Downloads/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#        sudo auditctl -w /home/$i/$documents/aaaaaaaa.txt -p wa -k mush 2>/dev/null
-#done
-#
-#IDENTIFICANDO RANSOMWARE E MATANDO ===========================|
-
 while true; do
       PID=$(sudo ausearch -k mush | grep -i ppid | tail -n 1 | grep -o 'pid=[0-9]*' | awk -F'=' '{print $2}')
       if [ -e "./SINALIZADOR.dat" ]; then
