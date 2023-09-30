d06881c9e9d4057ada562f7967c26e174f90dc90
JosucaZ <102182842+JosucaZ@users.noreply.github.com>
2023-09-29 21:11:28 -0300

    Update desinstalador.sh

--- desinstalador.sh
+++ desinstalador.sh
@@ -36,20 +36,27 @@ if [ "$option" = "1" ]; then
     systemctl daemon-reload
 
 elif [ "$option" = "2" ]; then
-    echo -e "\nDESINSTALANDO HONEYFILES..."
-    if [ -e /$nomearq ] && [ -e /home/$nomearq ] && [ -e /etc/$nomearq ] && [ -e /usr/$nomearq ] &&  [ -e /backup/$nomearq ]; then
-        echo "Parando serviços selecionados."
-        echo "~# systemctl stop challenge.service"
-        systemctl stop challenge.service
-        echo "~# systemctl stop backup.service"
-        systemctl stop backup.service
-        echo "~# systemctl stop flushlog.service"
-        systemctl stop flushlog.service
-        echo "~# systemctl stop instalador.service"
-        systemctl stop instalador.service
-        echo "~# systemctl daemon-reload"
-        systemctl daemon-reload
+    echo "Parando serviços selecionados."
+    echo "~# systemctl stop challenge.service"
+    systemctl stop challenge.service
+    echo "~# systemctl stop backup.service"
+    systemctl stop backup.service
+    echo "~# systemctl stop flushlog.service"
+    systemctl stop flushlog.service
+    echo "~# systemctl stop instalador.service"
+    systemctl stop instalador.service
 
+    echo "~# systemctl disable challenge.service"
+    systemctl disable challenge.service
+    echo "~# systemctl disable backup.service"
+    systemctl disable backup.service
+    echo "~# systemctl disable flushlog.service"
+    systemctl disable flushlog.service
+    echo "~# systemctl disable instalador.service"
+    systemctl disable instalador.service
+        
+    echo -e "\nDESINSTALANDO HONEYFILES..."
+    if [ -e /$nomearq ] && [ -e /home/$nomearq ] && [ -e /etc/$nomearq ] && [ -e /usr/$nomearq ] &&  [ -e /backup/$nomearq ]; then        
         echo "~# sudo rm -rf /$nomearq"
         sudo rm -rf /$nomearq
         echo "~# sudo rm -rf /home/$nomearq"
@@ -60,27 +67,58 @@ elif [ "$option" = "2" ]; then
         sudo rm -rf /usr/$nomearq
         echo "~# sudo rm -rf /backup/$nomearq"
         sudo rm -rf /backup/$nomearq
+    fi
 
-        usuarios=$(cat /etc/passwd | grep -i /home | cut -d: -f1)
-        for i in ${usuarios[@]}; do
-            echo "~# sudo -u $i rm /home/$i/$nomearq"
-            sudo -u $i rm /home/$i/$nomearq
-            echo "~# sudo -u $i rm /home/$i/$documents/$nomearq"
-            sudo -u $i rm /home/$i/$documents/$nomearq
-            echo "~# sudo -u $i rm /home/$i/Downloads/$nomearq"
-            sudo -u $i rm /home/$i/Downloads/$nomearq
-            echo "~# sudo -u $i rm "$desktop/$nomearq""
-            sudo -u $i rm "$desktop/$nomearq"
-            echo "~# sudo -u $i rm /home/$i/$videos/$nomearq"
-            sudo -u $i rm /home/$i/$videos/$nomearq
-        done
-        echo -e "\n--------------------------------------------"
-        echo "Honeyfiles desinstalados com sucesso!"
+    usuarios=$(cat /etc/passwd | grep -i /home | cut -d: -f1)
+    for i in ${usuarios[@]}; do
+        echo "~# rm -rf /home/$i/$nomearq"
+        rm -rf /home/$i/$nomearq
+        echo "~# rm -rf /home/$i/$documents/$nomearq"
+        rm -rf /home/$i/$documents/$nomearq
+        echo "~# rm -rf /home/$i/Downloads/$nomearq"
+        rm -rf /home/$i/Downloads/$nomearq
+        echo "~# rm -rf "$desktop/$nomearq""
+        rm -rf "$desktop/$nomearq"
+        echo "~# rm -rf /home/$i/$videos/$nomearq"
+        rm -rf /home/$i/$videos/$nomearq
+    done
+
+    localArqs=$(find /home -name "$nomearq" 2>/dev/null)
+    for i in $localArqs; do
+        echo "~# rm -rf $i"
+        rm -rf $i
+    done
+        
+    echo -e "\n--------------------------------------------"
+    echo "Honeyfiles desinstalados com sucesso!"
+    echo -e "\nRemovendo .services"
+    echo "~# rm -rf /etc/systemd/system/challange.service"
+    rm -rf /etc/systemd/system/challange.service
+    echo "~# /etc/systemd/system/backup.service"
+    rm -rf /etc/systemd/system/backup.service
+    echo "~# /etc/systemd/system/flushlog.service"
+    rm -rf /etc/systemd/system/flushlog.service
+    echo "~# /etc/systemd/system/instalador.service"
+    rm -rf /etc/systemd/system/instalador.service
+
+    echo -e "Removendo scripts"
+    echo "~# rm -rf /usr/sbin/challange.sh"
+    rm -rf /usr/sbin/challange.sh
+    echo "~# /usr/sbin/backup.sh"
+    rm -rf /usr/sbin/backup.sh
+    echo "~# /usr/sbin/flushlog.sh"
+    rm -rf /usr/sbin/flushlog.sh
+    echo "~# /usr/sbin/instalador.sh"
+    rm -rf /usr/sbin/instalador.sh
+
+        
     else
-        echo "Honeyfiles não encontrados ou não estão no tamanho esperado."
+        nada="nada"
     fi
     echo "~# systemctl daemon-reload"
     systemctl daemon-reload
+
+    echo -e "~# Desintalação concluida!"
 else
     echo "Opção inválida."
 fi
