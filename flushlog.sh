a6092cf211943534b4b5f06873e82b11ba781cb6
JosucaZ <102182842+JosucaZ@users.noreply.github.com>
2023-10-02 14:50:26 -0300

    Update flushlog.sh

--- flushlog.sh
+++ flushlog.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Autores: 
+# Erick Rorato
+# Josué Suptitz
+# github.com/erickroratome/Mushroom
+
+echo -e "Anti-Ransomware - By:"
+echo -e "\e[91m  _  __           _                                   \e[0m"
+echo -e "\e[91m|  \/  |_   _ ___| |__  _ __ ___ ___  _ __ ___  ___ \e[0m"
+echo -e "\e[91m| |\/| | | | / __| '_ \| '__/ _ \ _ \| '_ \` _ \/ __|\e[0m"
+echo -e "\e[91m| |  | | |_| \__ \ | | | | | (_) (_) | | | | | \__ \\\ \e[0m"
+echo -e "\e[91m|_|  |_|\__,_|___/_| |_|_|  \___/___/|_| |_| |_|___/\e[0m"
 
 
 while true; do
